4a36dd26cf6d8b3ea581b8632c4cb142c1cd622e
Andrew Brooks <andrew.brooks@talktalkplc.com>
2014-06-06 11:43:19 +0100

    Add getters/setters to gallery item

--- main/java/com/andy/photogallery/model/GalleryItem.java
+++ main/java/com/andy/photogallery/model/GalleryItem.java
@@ -10,4 +10,28 @@ public class GalleryItem {
         return caption;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
 }
