5e2744fc91f6c18b112c4f4c3b5269bd186d21d7
Andrew Brooks <andrew.brooks@talktalkplc.com>
2014-06-06 12:49:06 +0100

    Make gallery item immutable

--- main/java/com/andy/photogallery/model/GalleryItem.java
+++ main/java/com/andy/photogallery/model/GalleryItem.java
@@ -6,32 +6,26 @@ public class GalleryItem {
     private String url;
     private String caption;
 
-    public String toString() {
-        return caption;
+    public GalleryItem(String id, String url, String caption) {
+        this.id = id;
+        this.url = url;
+        this.caption = caption;
     }
 
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getUrl() {
         return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
     }
 
     public String getCaption() {
         return caption;
     }
 
-    public void setCaption(String caption) {
-        this.caption = caption;
+    public String toString() {
+        return caption;
     }
 
 }
