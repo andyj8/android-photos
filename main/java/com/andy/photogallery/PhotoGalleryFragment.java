e679349d0bf6b5c05d7815ceed04547603f3c872
Andrew Brooks <andrew.brooks@talktalkplc.com>
2014-06-06 13:06:47 +0100

    Add adapter

--- main/java/com/andy/photogallery/PhotoGalleryFragment.java
+++ main/java/com/andy/photogallery/PhotoGalleryFragment.java
@@ -3,18 +3,20 @@ package com.andy.photogallery;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import com.andy.photogallery.model.GalleryItem;
+import com.andy.photogallery.model.GalleryItemRepository;
 
-import java.io.IOException;
+import java.util.ArrayList;
 
 public class PhotoGalleryFragment extends Fragment {
 
-    private static final String TAG = "PhotoGalleryFragment";
     private GridView mGridView;
+    private ArrayList<GalleryItem> mItems;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,16 +30,35 @@ public class PhotoGalleryFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
         mGridView = (GridView) v.findViewById(R.id.grid_view);
-
+        initAdapter();
         return v;
     }
 
-    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
-
+    private class FetchItemsTask extends AsyncTask<Void, Void, ArrayList<GalleryItem>> {
         @Override
-        protected Void doInBackground(Void... params) {
-            new FlickrFetcher().fetchItems();
-            return null;
+        protected ArrayList<GalleryItem> doInBackground(Void... params) {
+            return new GalleryItemRepository().getItems();
+        }
+        @Override
+        protected void onPostExecute(ArrayList<GalleryItem> items) {
+            mItems = items;
+            initAdapter();
+        }
+    }
+
+    private void initAdapter() {
+        if (getActivity() == null || mGridView == null) {
+            return;
+        }
+
+        if (mItems != null) {
+            mGridView.setAdapter(
+                new ArrayAdapter<GalleryItem>(
+                    getActivity(), android.R.layout.simple_gallery_item, mItems
+                )
+            );
+        } else {
+            mGridView.setAdapter(null);
         }
     }
 
