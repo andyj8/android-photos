50f2c6a68bf1a1d6a20718c2054e2c4b13d1e433
Andrew Brooks <andrew.brooks@talktalkplc.com>
2014-06-06 11:21:35 +0100

    Move fragment to separate class

--- main/java/com/andy/photogallery/PhotoGalleryActivity.java
+++ main/java/com/andy/photogallery/PhotoGalleryActivity.java
@@ -1,11 +1,7 @@
 package com.andy.photogallery;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 public class PhotoGalleryActivity extends ActionBarActivity {
 
@@ -15,20 +11,9 @@ public class PhotoGalleryActivity extends ActionBarActivity {
         setContentView(R.layout.activity_photo_gallery);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new PhotoGalleryFragment())
                     .commit();
         }
     }
 
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_photo_gallery, container, false);
-        }
-    }
 }
---
+++ main/java/com/andy/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,49 @@
+package com.andy.photogallery;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import java.io.IOException;
+
+public class PhotoGalleryFragment extends Fragment {
+
+    private static final String TAG = "PhotoGalleryFragment";
+    private GridView mGridView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+        new FetchItemsTask().execute();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+        mGridView = (GridView) v.findViewById(R.id.grid_view);
+
+        return v;
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String result = new FlickrFetcher().getUrl("http://www.google.com");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Failed to fetch URL: ", ioe);
+            }
+            return null;
+        }
+    }
+
+}
