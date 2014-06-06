84a71d6773f27c743649ffa53f1a440f712a546e
Andrew Brooks <andrew.brooks@talktalkplc.com>
2014-06-06 14:49:08 +0100

    Add custom gallery adapter to fragment

--- main/java/com/andy/photogallery/PhotoGalleryFragment.java
+++ main/java/com/andy/photogallery/PhotoGalleryFragment.java
@@ -3,26 +3,45 @@ package com.andy.photogallery;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 import com.andy.photogallery.model.GalleryItem;
 import com.andy.photogallery.model.GalleryItemRepository;
+import com.andy.photogallery.service.ThumbnailDownloader;
 
 import java.util.ArrayList;
 
 public class PhotoGalleryFragment extends Fragment {
 
+    private static final String TAG = "PhotoGalleryFragment";
+
     private GridView mGridView;
     private ArrayList<GalleryItem> mItems;
+    private ThumbnailDownloader<ImageView> mThumbnailThread;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+
         new FetchItemsTask().execute();
+
+        mThumbnailThread = new ThumbnailDownloader<ImageView>(TAG);
+        mThumbnailThread.start();
+        mThumbnailThread.getLooper();
+        Log.i(TAG, "STARTED");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mThumbnailThread.quit();
+        Log.i(TAG, "DESTROYED");
     }
 
     @Override
@@ -52,14 +71,32 @@ public class PhotoGalleryFragment extends Fragment {
         }
 
         if (mItems != null) {
-            mGridView.setAdapter(
-                new ArrayAdapter<GalleryItem>(
-                    getActivity(), android.R.layout.simple_gallery_item, mItems
-                )
-            );
+            mGridView.setAdapter(new GalleryItemAdapter(mItems));
         } else {
             mGridView.setAdapter(null);
         }
     }
 
+    private class GalleryItemAdapter extends ArrayAdapter<GalleryItem> {
+
+        public GalleryItemAdapter(ArrayList<GalleryItem> items) {
+            super(getActivity(), 0, items);
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup parent) {
+            if (view == null) {
+                LayoutInflater inflater = getActivity().getLayoutInflater();
+                view = inflater.inflate(R.layout.gallery_item, parent, false);
+            }
+
+            ImageView imageView = (ImageView) view.findViewById(R.id.gallery_item_image);
+            imageView.setImageResource(R.drawable.kermit);
+
+            GalleryItem item = getItem(position);
+            mThumbnailThread.queueThumbnail(imageView, item.getUrl());
+
+            return view;
+        }
+    }
 }
