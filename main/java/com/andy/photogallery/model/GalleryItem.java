f89e44cbc43eba72104d5af2b02333e2b23ae7a2
Andrew Brooks <andrew.brooks@talktalkplc.com>
2014-06-06 11:42:39 +0100

    Add gallery item model

---
+++ main/java/com/andy/photogallery/model/GalleryItem.java
@@ -0,0 +1,13 @@
+package com.andy.photogallery.model;
+
+public class GalleryItem {
+
+    private String id;
+    private String url;
+    private String caption;
+
+    public String toString() {
+        return caption;
+    }
+
+}
