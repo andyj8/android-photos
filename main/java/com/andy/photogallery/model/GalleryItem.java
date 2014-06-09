64869af0675794c3b138f0674ef38d8182d8a746
Andrew Brooks <andrew.brooks@talktalkplc.com>
2014-06-09 12:10:18 +0100

    Add filename getter

--- main/java/com/andy/photogallery/model/GalleryItem.java
+++ main/java/com/andy/photogallery/model/GalleryItem.java
@@ -1,15 +1,26 @@
 package com.andy.photogallery.model;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class GalleryItem {
 
     private String id;
     private String url;
     private String caption;
+    private String file;
 
     public GalleryItem(String id, String caption, String url) {
         this.id = id;
         this.caption = caption;
         this.url = url;
+
+        try {
+            URL urlInfo = new URL(getUrl());
+            this.file = urlInfo.getFile();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
 
     public String getId() {
@@ -20,6 +31,10 @@ public class GalleryItem {
         return url;
     }
 
+    public String getFile() {
+        return file;
+    }
+
     public String getCaption() {
         return caption;
     }
@@ -27,5 +42,7 @@ public class GalleryItem {
     public String toString() {
         return caption;
     }
+
+
 
 }
