28f58ae59e990faddecf87bafba5a70816c5e84a
Andrew Brooks <andrew.brooks@talktalkplc.com>
2014-06-09 15:06:22 +0100

    Remove item file property

--- main/java/com/andy/photogallery/model/GalleryItem.java
+++ main/java/com/andy/photogallery/model/GalleryItem.java
@@ -1,26 +1,15 @@
 package com.andy.photogallery.model;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
 public class GalleryItem {
 
     private String id;
     private String url;
     private String caption;
-    private String file;
 
     public GalleryItem(String id, String caption, String url) {
         this.id = id;
         this.caption = caption;
         this.url = url;
-
-        try {
-            URL urlInfo = new URL(getUrl());
-            this.file = urlInfo.getFile();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
     }
 
     public String getId() {
@@ -31,10 +20,6 @@ public class GalleryItem {
         return url;
     }
 
-    public String getFile() {
-        return file;
-    }
-
     public String getCaption() {
         return caption;
     }
@@ -42,7 +27,5 @@ public class GalleryItem {
     public String toString() {
         return caption;
     }
-
-
 
 }
